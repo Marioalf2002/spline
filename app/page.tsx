ed71a53c22dc2eddbcd5b471256251a919518d13
Mario Hernandez <52926248+Marioalf2002@users.noreply.github.com>
2024-05-30 12:09:15 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         </div>
         <div className="w-full h-2/4 md:h-full md:w-3/5 flex items-center justify-center relative -z-10">
           <Spline
-            className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[1] items-center justify-center md:justify-start"
+            className="w-full flex scale-[.70] lg:scale-[1] items-center justify-center md:justify-start"
             scene="https://prod.spline.design/zrDbpTm6b8Xyml6n/scene.splinecode"
           />
         </div>
